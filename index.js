bd27c3569c5559d4ec548dc7ee96fc58d0c70661
yobe8 <64393019+yobe8@users.noreply.github.com>
2020-09-19 22:03:26 +0900

    finished background changer maybe?
    갑자기 끝나서 놀람

---
+++ index.js
@@ -0,0 +1,31 @@
+const hexText = document.querySelector('.hexcolor-text__colors'),
+hexBtn = document.getElementById('random-btn'),
+body = document.querySelector('body');
+
+function randomHexColor() {
+    const NUM_AND_ALPHABET = '1234567890abcdef' //length = 16
+    const CHAR_BOX = NUM_AND_ALPHABET.split('').sort();
+    let hexColor = '';
+
+    for(i= 0; i < 6; i++) {
+        hexColor = hexColor.concat(CHAR_BOX[Math.floor(Math.random()*16)]);
+    }
+
+    return hexColor;
+};
+
+function sendHexCodes() {
+    let hexCodes = randomHexColor();
+    hexText.innerText = `#${hexCodes}`;
+    body.style.backgroundColor= `#${hexCodes}`;
+}
+
+function handleBtn() {
+    hexBtn.addEventListener('click', sendHexCodes);
+}
+
+function init() {
+    handleBtn();
+}
+
+init();
